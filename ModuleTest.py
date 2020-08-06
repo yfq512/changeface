619f073485874d07073b7648087ff01ea3b63aa8
yfq512 <1085561337@qq.com>
2020-08-06 08:58:08 +0800

    run

--- ModuleTest.py
+++ ModuleTest.py
@@ -5,8 +5,8 @@
 import core
 
 if __name__ == '__main__':
-    core.face_merge(src_img='images/3.jpg',
-                    dst_img='images/model.jpg',
+    core.face_merge(src_img='images/1.jpg',
+                    dst_img='images/2.jpg',
                     out_img='images/output.jpg',
                     face_area=[50, 30, 500, 485],
                     alpha=0.75,
