27b691771563fb06b9b18d0415e577e21d5fb482
yfq512 <1085561337@qq.com>
2020-12-16 09:34:29 +0800

    用nginx将图像进行反向代理

--- server.py
+++ server.py
@@ -19,48 +19,53 @@ app = Flask(__name__)
 
 def face_merge():
     if request.method == "POST":
-        imgdata1 = request.form.get('imgdata1')
-        imgname1 = request.form.get('imgname1')
-        imgdata2 = request.form.get('imgdata2')
-        imgname2 = request.form.get('imgname2')
-        alpha = request.form.get('alpha') # 0.85
-        mat_multiple = request.form.get('mat_multiple') # 0.9
+        try:
+            imgdata1 = request.form.get('imgdata1')
+            imgname1 = request.form.get('imgname1')
+            imgdata2 = request.form.get('imgdata2')
+            imgname2 = request.form.get('imgname2')
+            alpha = request.form.get('alpha') # 0.85
+            mat_multiple = request.form.get('mat_multiple') # 0.9
 
-        alpha = float(alpha)
-        mat_multiple = float(mat_multiple)
+            alpha = float(alpha)
+            mat_multiple = float(mat_multiple)
 
-        temp_root = 'temp'
+            temp_root = 'temp'
 
-        imgdata1 = re.sub(r'data:image/[a-zA-Z]*;base64,', "",imgdata1) # 适配js库base64
-        imgdata1 = imgdata1.replace("data:image/jpeg;base64,", "") # 适配多种图像类型
-        imgdata1 = base64.b64decode(imgdata1)
-        img_stye1 = '.' + imgname1.split('.')[-1]
-        img_path_temp1 = getRandomSet(15) + img_stye1
-        img_path_temp1 = os.path.join(temp_root, img_path_temp1)
-        f1 = open(img_path_temp1, 'wb')
-        f1.write(imgdata1)
-        f1.close()
+            imgdata1 = re.sub(r'data:image/[a-zA-Z]*;base64,', "",imgdata1) # 适配js库base64
+            imgdata1 = imgdata1.replace("data:image/jpeg;base64,", "") # 适配多种图像类型
+            imgdata1 = base64.b64decode(imgdata1)
+            img_stye1 = '.' + imgname1.split('.')[-1]
+            img_path_temp1 = getRandomSet(15) + img_stye1
+            img_path_temp1 = os.path.join(temp_root, img_path_temp1)
+            f1 = open(img_path_temp1, 'wb')
+            f1.write(imgdata1)
+            f1.close()
 
-        imgdata2 = re.sub(r'data:image/[a-zA-Z]*;base64,', "",imgdata2) # 适配js库base64
-        imgdata2 = imgdata2.replace("data:image/jpeg;base64,", "") # 适配多种图像类型
-        imgdata2 = base64.b64decode(imgdata2)
-        img_stye2 = '.' + imgname2.split('.')[-1]
-        img_path_temp2 = getRandomSet(15) + img_stye2
-        img_path_temp2 = os.path.join(temp_root, img_path_temp2)
-        f2 = open(img_path_temp2, 'wb')
-        f2.write(imgdata2)
-        f2.close()
+            imgdata2 = re.sub(r'data:image/[a-zA-Z]*;base64,', "",imgdata2) # 适配js库base64
+            imgdata2 = imgdata2.replace("data:image/jpeg;base64,", "") # 适配多种图像类型
+            imgdata2 = base64.b64decode(imgdata2)
+            img_stye2 = '.' + imgname2.split('.')[-1]
+            img_path_temp2 = getRandomSet(15) + img_stye2
+            img_path_temp2 = os.path.join(temp_root, img_path_temp2)
+            f2 = open(img_path_temp2, 'wb')
+            f2.write(imgdata2)
+            f2.close()
 
-        out_img_path = os.path.join(temp_root, getRandomSet(15) + '.jpg')
+            rand_name = getRandomSet(15) + '.jpg'
+            out_img_path = os.path.join('/opt/docker/python/imgs_facemerge', rand_name)
 
-        ModuleTest2.main(img_path_temp1, img_path_temp2, out_img_path, alpha, mat_multiple)
-        os.remove(img_path_temp1)
-        os.remove(img_path_temp2)
-        return {'sign':1, 'outimgname':out_img_path}
+            ModuleTest2.main(img_path_temp1, img_path_temp2, out_img_path, alpha, mat_multiple)
+            os.remove(img_path_temp1)
+            os.remove(img_path_temp2)
+            res = 'http://192.168.132.151:8801/' + 'imgs_facemerge/' + rand_name
+            return {'sign':1, 'outimg':res}
+        except:
+            return {'sign':-1, 'outimg':''}
     else:
         return "<h1>Face merge!</h1>"
 
 if __name__ == '__main__':
-    host = '127.0.0.1'
-    port = '8881'
+    host = '0.0.0.0'
+    port = '8876'
     app.run(debug=True, host=host, port=port)
